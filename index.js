4f64f4f58df55c64b8a7bed8e947acf8dfa2bfa5
Dickson25-dot <dicksonndereba25@gmail.com>
2025-03-11 00:17:42 +0300

    complete assignment

--- index.js
+++ index.js
@@ -7,34 +7,6 @@
 // drivers()
 //   
 // returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']) => ['Antonia', 'Nuru']
-//   
- // Returns the last two drivers from the array
-//   function returnLastTwoDrivers(drivers) {
-    // return drivers.slice(-2);
-//   }
-//   
- // Array containing the two functions above
-//   const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
-//   
- // Returns a function that multiplies a fare by a given multiplier
-//   function createFareMultiplier(multiplier) {
-    // return function(fare) {
-    //   return fare * multiplier;
-    // };
-//   }
-//   
- // Doubles the fare
-//   const fareDoubler = createFareMultiplier(2);
-//   
- // Triples the fare
-//   const fareTripler = createFareMultiplier(3);
-//   
- // Selects different drivers based on the provided function
-//   function selectDifferentDrivers(arrayOfDrivers, driverSelector) {
-    // return driverSelector(arrayOfDrivers);
-//   }
-// 
-
 
 let drivers=(['Antonia', 'Nuru', 'Amari', 'Mo'])
 let returnFirstTwoDrivers = (drivers) => drivers.slice(0, 2);
